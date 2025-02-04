b2b234ce4ad9a16eb312cccdfbfe4bbf69aba46c
UBC Student <lam.michael.81201@gmail.com>
2025-02-04 15:40:18 -0800

    Added springboot initial files

---
+++ genshin-simulator-backend/src/main/java/michlam/genshin_simulator_backend/GenshinSimulatorBackendApplication.java
@@ -0,0 +1,13 @@
+package michlam.genshin_simulator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinSimulatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinSimulatorBackendApplication.class, args);
+	}
+
+}
