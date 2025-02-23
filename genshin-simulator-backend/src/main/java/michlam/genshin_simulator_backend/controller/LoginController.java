35b78d2423cb917688e291da67c7ffac1939620a
UBC Student <lam.michael.81201@gmail.com>
2025-02-23 14:59:36 -0800

    created a login api

---
+++ genshin-simulator-backend/src/main/java/michlam/genshin_simulator_backend/controller/LoginController.java
@@ -0,0 +1,23 @@
+package michlam.genshin_simulator_backend.controller;
+
+import lombok.AllArgsConstructor;
+import michlam.genshin_simulator_backend.dto.UserDto;
+import michlam.genshin_simulator_backend.service.LoginService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/login") // Used to denote the base URL for any user-based apis.
+public class LoginController {
+    private LoginService loginService;
+
+    // Build Get User By ID REST API
+    @GetMapping // Maps get to this method
+    public ResponseEntity<Boolean> login(@RequestBody UserDto userDto) {
+        String username = userDto.getUsername();
+        String password = userDto.getPassword();
+
+        return ResponseEntity.ok(loginService.login(username, password));
+    }
+}
