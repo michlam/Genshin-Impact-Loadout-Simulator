7405ef5de2107b61de609f7f3d76988996e7614f
UBC Student <lam.michael.81201@gmail.com>
2025-02-24 14:46:31 -0800

    footer content and styling

--- genshin-simulator-frontend/src/components/Footer.jsx
+++ genshin-simulator-frontend/src/components/Footer.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer>
-            <p>2025 - Made by <a href="https://github.com/michlam">Michael Lam</a>. Assets from HoYoverse. All rights reserved.</p>
+            <section>
+                <div className='footer-left'>
+                    <h4>GENSHIN LOADOUTS</h4>
+                    <h1>Experiment all of your team combinations!</h1>
+                    <p className='attribution'>
+                        2025 Michael Lam. Assets from HoYoverse. All rights reserved.
+                    </p>
+                </div>
+
+                <div className='footer-right'>
+                    <h4>Links</h4>
+                    <p>LinkedIn</p>
+                    <p>GitHub</p>
+                </div>
+            </section>
         </footer>
     )
 }
--- genshin-simulator-frontend/src/components/Header.jsx
+++ genshin-simulator-frontend/src/components/Header.jsx
@@ -3,14 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-        <>
-            <header>
-                <Link className="site-logo" to="/">GENSHIN LOADOUTS</Link>
-                <button>Sign in</button>
-                
-            </header>
-            <hr className="header-rule"/>
-        </>
-
+        <header>
+            <Link className="site-logo" to="/">GENSHIN LOADOUTS</Link>
+            <button>Sign in</button>
+            
+        </header>
     )
 }
