af248b423da2b9ee6e243e09e7ca8d6d739d3369
UBC Student <lam.michael.81201@gmail.com>
2025-02-24 15:24:20 -0800

    Fixed main page sizing, started more routing

--- genshin-simulator-frontend/src/App.jsx
+++ genshin-simulator-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
+import NotFound from './pages/NotFound.jsx'
 
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<Layout />}>
 
+      <Route path="*" element={<NotFound />} />
     </Route>
 
   ))
---
+++ genshin-simulator-frontend/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="not-found">
+            <h1>The page you were looking for was not found.</h1>
+        </div>
+    )
+}
