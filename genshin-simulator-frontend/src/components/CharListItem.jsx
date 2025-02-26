0b50dc4a080ee43fbf678031e8e47b895fbf3466
UBC Student <lam.michael.81201@gmail.com>
2025-02-26 15:00:52 -0800

    Added character nametags

--- genshin-simulator-frontend/src/components/CharListItem.jsx
+++ genshin-simulator-frontend/src/components/CharListItem.jsx
@@ -2,12 +2,16 @@ export default function CharListItem(props) {
     const imagePath = "../../characters/avatars/ui-avataricon-" + props.name.toLowerCase() + ".png";
     const bgClass = props.star == 5 ? "five-star" : "four-star";
 
+    // We will omit any family names
+    let name = props.name.split(" ");
+    name = name[name.length - 1];
+
     return (
         <div className="char-list-item">
             <div className={`char-img-container ${bgClass}`}>
                 <img src={imagePath} />
+                <p>{name}</p>
             </div>
-            {props.name}
         </div>
     )
 }
