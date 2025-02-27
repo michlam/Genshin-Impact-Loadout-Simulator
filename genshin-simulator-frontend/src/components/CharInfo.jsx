9fc71487b759c558b85f5bbe333eb9962734d214
UBC Student <lam.michael.81201@gmail.com>
2025-02-27 12:29:31 -0800

    Styling for char info panel

--- genshin-simulator-frontend/src/components/CharInfo.jsx
+++ genshin-simulator-frontend/src/components/CharInfo.jsx
@@ -5,6 +5,11 @@ export default function CharInfo(props) {
     const imagePath = "../../characters/splasharts/" + 
         char.name.split(" ").join("_") + 
         "_Wish.webp";
+
+    let starsElement = "";
+    for (let i = 0; i < char.star; i++) {
+        starsElement += "⭐";
+    }
     
 
     return (
@@ -12,6 +17,20 @@ export default function CharInfo(props) {
             <div className="char-info-container">
                 <h2>{char.name}</h2>
                 <h4>{char.title}</h4>
+                <div className="char-info-details">
+                    <section>
+                        <h4>Rarity</h4>
+                        <p>{starsElement}</p>
+                    </section>
+                    <section>
+                        <h4>Element</h4>
+                        <p>{char.element}</p>
+                    </section>
+                    <section>
+                        <h4>Weapon</h4>
+                        <p>{char.weapon_type}</p>
+                    </section>
+                </div>
                 <img src={imagePath} alt={`${char.name} splashart`}/>
             </div>
         </div>
