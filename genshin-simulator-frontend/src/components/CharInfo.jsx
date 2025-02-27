677fbb56448410e4bef2a3ad586f6deeafaf1ae8
UBC Student <lam.michael.81201@gmail.com>
2025-02-26 21:31:49 -0800

    Added retrieve char info helper

--- genshin-simulator-frontend/src/components/CharInfo.jsx
+++ genshin-simulator-frontend/src/components/CharInfo.jsx
@@ -1,9 +1,23 @@
 import "./CharInfo.css";
 
 export default function CharInfo(props) {
+    const char = getCharacterInfo(props.charFocus, props.baseCharacters);
+
     return (
         <div className="char-info-container">
-            <p>Hello</p>
+            <p>{char.name}</p>
         </div>
     )
+}
+
+/**
+ * Helper function to retrieve the information for the given charName. 
+ * Return an object with name, title, star, element, and weapon_type
+ */
+function getCharacterInfo(charName, baseCharacters) {
+    for (let i = 0; i < baseCharacters.length; i++) {
+        if (baseCharacters[i].name === charName) return baseCharacters[i];
+    }
+
+    throw Error("Character focus does not exist.");
 }
