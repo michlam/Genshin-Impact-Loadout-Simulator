093e479ab7fcccd4b621a353e820a2ed2b955a7e
UBC Student <lam.michael.81201@gmail.com>
2025-03-01 14:04:45 -0800

    starter selector list item

--- genshin-simulator-frontend/src/components/Selector.jsx
+++ genshin-simulator-frontend/src/components/Selector.jsx
@@ -1,7 +1,35 @@
+import "./Selector.css";
+
 export default function Selector(props) {
+    const userCharacters = props.userCharacters;
+    const baseCharacters = props.baseCharacters;
+
+    function renderCharacters() {
+        const charElements = baseCharacters.map((char) => {
+            if (!userCharacters.includes(char.name)) return null;
+            // We have all the base characters that the user actually owns now.
+
+            const imagePath = "../../characters/avatars/ui-avataricon-" + char.name.toLowerCase() + ".png";
+            const bgClass = char.star == 5 ? "five-star" : "four-star";
+        
+            let name = char.name.split(" ");
+            name = name[name.length - 1];
+
+            return (
+                <div className={`selector-list-item ${bgClass}`}>
+                    <img src={imagePath}/>
+                </div>
+            )
+        })
+
+        return charElements;
+    }
+
     return (
-        <>
+        <fieldset className="selector-container">
+            <legend>Character Select</legend>
             Selector: {props.focus.teamNum}, {props.focus.charNum}
-        </>
+            {renderCharacters(props.focus.teamNum, props.focus.charNum)}
+        </fieldset>
     )
-}
+}    
