e87abaac71ab7c37f27ad796b490f428e7e726ad
UBC Student <lam.michael.81201@gmail.com>
2025-03-01 14:39:48 -0800

    added selected indicator to character select items

--- genshin-simulator-frontend/src/components/Selector.jsx
+++ genshin-simulator-frontend/src/components/Selector.jsx
@@ -4,19 +4,23 @@ export default function Selector(props) {
     const userCharacters = props.userCharacters;
     const baseCharacters = props.baseCharacters;
 
-    function renderCharacters() {
+    function renderCharacters(teamNum, charNum) {
+        const userTeam = props.userTeams[teamNum - 1];
+        const teamChars = [userTeam.character_name_1, userTeam.character_name_2, userTeam.character_name_3, userTeam.character_name_4];
+
         const charElements = baseCharacters.map((char) => {
             if (!userCharacters.includes(char.name)) return null;
             // We have all the base characters that the user actually owns now.
 
             const imagePath = "../../characters/avatars/ui-avataricon-" + char.name.toLowerCase() + ".png";
             const bgClass = char.star == 5 ? "five-star" : "four-star";
+            const selectedClass = teamChars.includes(char.name) ? "selected" : "";
         
             let name = char.name.split(" ");
             name = name[name.length - 1];
 
             return (
-                <div className={`selector-list-item ${bgClass}`}>
+                <div className={`selector-list-item ${bgClass} ${selectedClass}`}>
                     <img src={imagePath}/>
                 </div>
             )
@@ -28,7 +32,6 @@ export default function Selector(props) {
     return (
         <fieldset className="selector-container">
             <legend>Character Select</legend>
-            Selector: {props.focus.teamNum}, {props.focus.charNum}
             {renderCharacters(props.focus.teamNum, props.focus.charNum)}
         </fieldset>
     )
