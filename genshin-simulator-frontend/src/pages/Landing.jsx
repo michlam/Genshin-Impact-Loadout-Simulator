e545ff87767c1a345d0eebc1acaa72c74ea2dcec
UBC Student <lam.michael.81201@gmail.com>
2025-03-02 14:25:13 -0800

    worked on main landing page

--- genshin-simulator-frontend/src/pages/Landing.jsx
+++ genshin-simulator-frontend/src/pages/Landing.jsx
@@ -1,7 +1,17 @@
+import "./Landing.css";
+
 export default function Landing() {
     return (
-        <main>
-            Landing page
+        <main className="landing">
+            <div className="landing-content">
+                <h1>
+                    Optimize your Genshin Impact team loadouts and builds.
+                </h1>
+
+                <button>
+                    Customize your teams
+                </button>
+            </div>
         </main>
     )
 }
