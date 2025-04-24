8f6a42ff7f0fce9f1286c6a012e72eaa6c9bceb4
UBC Student <lam.michael.81201@gmail.com>
2025-04-24 09:01:31 -0700

    add authentication manager bean

--- genshin-simulator-backend/src/main/java/michlam/genshin_simulator_backend/config/ApplicationConfig.java
+++ genshin-simulator-backend/src/main/java/michlam/genshin_simulator_backend/config/ApplicationConfig.java
@@ -4,9 +4,10 @@ import lombok.RequiredArgsConstructor;
 import michlam.genshin_simulator_backend.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,6 +32,11 @@ public class ApplicationConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
