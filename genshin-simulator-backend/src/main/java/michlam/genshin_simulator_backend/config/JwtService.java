222725fdcc5c576739ea8f93c9e047c44b8c77bf
UBC Student <lam.michael.81201@gmail.com>
2025-04-23 20:39:27 -0700

    added token validation methods

--- genshin-simulator-backend/src/main/java/michlam/genshin_simulator_backend/config/JwtService.java
+++ genshin-simulator-backend/src/main/java/michlam/genshin_simulator_backend/config/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -47,6 +48,26 @@ public class JwtService {
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+
+
+    
+    // Validate the token belongs to user
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+
 
     private Claims extractAllClaims(String token) {
         return Jwts
