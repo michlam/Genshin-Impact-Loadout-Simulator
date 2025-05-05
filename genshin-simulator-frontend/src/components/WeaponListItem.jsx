531ecfc1f385e33807b6de170a45246ae81fbbbf
UBC Student <lam.michael.81201@gmail.com>
2025-05-05 11:01:34 -0700

    Completed the weapons list

--- genshin-simulator-frontend/src/components/WeaponListItem.jsx
+++ genshin-simulator-frontend/src/components/WeaponListItem.jsx
@@ -1,15 +1,6 @@
 export default function WeaponListItem(props) {
-    // TODO: NEED MORE PREP WORK FOR THE IMAGE PATH (ex: remove apostrophes)
-    let imagePath = "../../characters/avatars/ui-avataricon-" + props.name.toLowerCase() + ".png";
-
-    let bgClass;
-    if (props.star == 5) bgClass = "five-star";
-    if (props.star == 4) bgClass = "four-star";
-    if (props.star == 3) bgClass = "three-star";
-
-    // We will omit any family names
-    let name = props.name.split(" ");
-    name = name[name.length - 1];
+    const imagePath = getImagePath(props.name);
+    const bgClass = getBgClass(props.star);
 
     return (
         <div className="weapon-list-item" onClick={() => props.setWeaponFocus(props.name)}>
@@ -19,3 +10,23 @@ export default function WeaponListItem(props) {
         </div>
     )
 }
+
+/**
+ * Helper function to retrieve the weapon's image path
+ */
+function getImagePath(weaponName) {
+    let name = weaponName.toLowerCase().split(" ").join("-");
+    let imagePath = "/weapons/icons/" + name + ".png";
+
+    return imagePath;
+}
+
+/**
+ * Helper function to retrieve the weapon's background based on the star
+ */
+function getBgClass(star) {
+    if (star == 5) return "five-star";
+    if (star == 4) return "four-star";
+    if (star == 3) return "three-star";
+}
+
