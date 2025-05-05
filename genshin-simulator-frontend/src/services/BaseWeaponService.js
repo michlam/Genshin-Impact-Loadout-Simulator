6875a87bac591ffe505269958a930029e680a80c
UBC Student <lam.michael.81201@gmail.com>
2025-05-05 10:12:15 -0700

    Create BaseWeaponService.js

---
+++ genshin-simulator-frontend/src/services/BaseWeaponService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const REST_API_BASE_URL = 'http://localhost:8080/api/base-weapons';
+
+export const getBaseWeapons = () => {
+    return axios.get(REST_API_BASE_URL,
+        {
+            headers: {
+                'Authorization': `Bearer ${sessionStorage.getItem("token")}`
+            }
+        });
+}
+
+export const getBaseWeaponByName = (name) => {
+    return axios.get(REST_API_BASE_URL + "/" + name,
+    {
+        headers: {
+            'Authorization': `Bearer ${sessionStorage.getItem("token")}`
+        }
+    });
+}
