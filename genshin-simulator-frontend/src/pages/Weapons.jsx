16aa9c461e37cc508559527d6af290c1200f0b40
UBC Student <lam.michael.81201@gmail.com>
2025-05-06 13:05:48 -0700

    Created getWeaponInfo helper function

--- genshin-simulator-frontend/src/pages/Weapons.jsx
+++ genshin-simulator-frontend/src/pages/Weapons.jsx
@@ -29,6 +29,8 @@ export default function Weapons() {
     const {baseWeapons} = useLoaderData();
     const [weaponFocus, setWeaponFocus] = useState(null);
 
+    const weapon = getWeaponInfo(weaponFocus, baseWeapons);
+
     return (
         <main className="weapons">
             <fieldset className="weapon-list">
@@ -37,4 +39,17 @@ export default function Weapons() {
             </fieldset>
         </main>
     )
+}
+
+/**
+ * Helper function to retrieve the information for the given weaponName. 
+ * Return an object with name, type, star, attack, secondary, 
+ * passiveName, and passiveText
+ */
+function getWeaponInfo(weaponName, baseWeapons) {
+    for (let i = 0; i < baseWeapons.length; i++) {
+        if (baseWeapons[i].name === weaponName) return baseWeapons[i];
+    }
+
+    return null;
 }
