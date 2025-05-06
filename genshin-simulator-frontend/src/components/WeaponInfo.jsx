ccdf3053b9274aa30977eae266fdfa2bc562df66
UBC Student <lam.michael.81201@gmail.com>
2025-05-06 13:27:50 -0700

    Started weaponInfo styling

--- genshin-simulator-frontend/src/components/WeaponInfo.jsx
+++ genshin-simulator-frontend/src/components/WeaponInfo.jsx
@@ -1,4 +1,4 @@
-import "./CharInfo.css";
+import "./WeaponInfo.css";
 
 export default function WeaponInfo(props) {
     const weapon = props.weapon;
@@ -11,25 +11,28 @@ export default function WeaponInfo(props) {
     const imagePath = getImagePath(weapon.name);
 
     return (
-        <div className="char-info-border">
-            <div className="char-info-container">
+        <div className="weapon-info-border">
+            <div className="weapon-info-container">
                 <h2>{weapon.name}</h2>
-                <h4>{weapon.name}</h4>
-                <div className="char-info-details">
+                <h4>{starsElement}</h4>
+                <div className="weapon-info-details">
                     <section>
-                        <h4>Rarity</h4>
-                        <p>{starsElement}</p>
-                    </section>
-                    <section>
-                        <h4>Element</h4>
+                        <h4>Type</h4>
                         <p>{weapon.type}</p>
                     </section>
                     <section>
-                        <h4>Weapon</h4>
+                        <h4>Attack</h4>
                         <p>{weapon.attack}</p>
+                    </section>
+                    <section>
+                        <h4>Secondary</h4>
+                        <p>{weapon.secondary}</p>
                     </section>
                 </div>
                 <img src={imagePath} alt={`${weapon.name} splashart`} loading="lazy"/>
+                <div className="weapon-passive">
+                    <h4>Passive Coming Soon</h4>
+                </div>
                 
             </div>
         </div>
